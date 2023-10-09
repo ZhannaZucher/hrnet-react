2874c7aa4333e98aced5f33ae8508cc22062283e
ZhannaZucher <jzucher@gmail.com>
2023-10-09 11:25:37 +0200

    add classNames

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -77,47 +77,46 @@ const Form = () => {
             <input type="text" />
           </label>
         </div>
-        <fieldset>
-          <legend>Address</legend>
-          <div>
-            <label>
-              Street
-              <input
-                type="text"
-                {...register("street", {
-                  required: "This field is required",
-                  pattern: {
-                    value: /[A-Za-z0-9]{5}/,
-                    message: "Enter at least 5 caracters",
-                  },
-                })}
-              />
-            </label>
-            {errors?.street && (
-              <p>{errors?.street?.message || "Street is required"}</p>
-            )}
-            <label>
-              City
-              <input
-                type="text"
-                {...register("city", {
-                  required: "This field is required",
-                  pattern: {
-                    value: /[A-Za-z]{2}/,
-                    message: "Enter at least 2 caracters",
-                  },
-                })}
-              />
-            </label>
-            {errors?.city && (
-              <p>{errors?.city?.message || "City is required"}</p>
-            )}
-            <label>
-              State
+
+        <div className={styles.flex}>
+          <label>
+            Street
+            <input
+              type="text"
+              {...register("street", {
+                required: "This field is required",
+                pattern: {
+                  value: /[A-Za-z0-9]{5}/,
+                  message: "Enter at least 5 caracters",
+                },
+              })}
+            />
+          </label>
+          {errors?.street && (
+            <p>{errors?.street?.message || "Street is required"}</p>
+          )}
+          <label>
+            City
+            <input
+              type="text"
+              {...register("city", {
+                required: "This field is required",
+                pattern: {
+                  value: /[A-Za-z]{2}/,
+                  message: "Enter at least 2 caracters",
+                },
+              })}
+            />
+          </label>
+          {errors?.city && <p>{errors?.city?.message || "City is required"}</p>}
+          <label>
+            State
+            <div className={styles.select_wrapper}>
               <select
                 {...register("state", {
                   required: "The state is required",
                 })}
+                className={styles.select}
               >
                 <option key="0" value="">
                   Select...
@@ -128,50 +127,56 @@ const Form = () => {
                   </option>
                 ))}
               </select>
-            </label>
-            {errors?.state && (
-              <p>{errors?.state?.message || "State is required"}</p>
-            )}
-            <label>
-              Zip Code
-              <input
-                type="text"
-                {...register("zipCode", {
-                  required: "This field is required",
-                  pattern: {
-                    value: /[0-9]{5}/,
-                    message: "Enter 5 digits",
-                  },
-                })}
-              />
-            </label>
-            {errors?.zipCode && (
-              <p>{errors?.zipCode?.message || "Zip Code is required"}</p>
-            )}
-          </div>
-        </fieldset>
+            </div>
+          </label>
+          {errors?.state && (
+            <p>{errors?.state?.message || "State is required"}</p>
+          )}
+          <label>
+            Zip Code
+            <input
+              type="number"
+              {...register("zipCode", {
+                required: "This field is required",
+                pattern: {
+                  value: /[0-9]{5}/,
+                  message: "Enter 5 digits",
+                },
+              })}
+            />
+          </label>
+          {errors?.zipCode && (
+            <p>{errors?.zipCode?.message || "Zip Code is required"}</p>
+          )}
+        </div>
       </div>
       <label>
         Department
-        <select
-          {...register("dept", {
-            required: "The department is required",
-          })}
-        >
-          <option key="0" value="">
-            Select...
-          </option>
-          {departments.map((dept) => (
-            <option key={dept} value={dept}>
-              {dept}
+        <div className={styles.select_wrapper}>
+          <select
+            {...register("dept", {
+              required: "The department is required",
+            })}
+            className={styles.select}
+          >
+            <option key="0" value="">
+              Select...
             </option>
-          ))}
-        </select>
+            {departments.map((dept) => (
+              <option key={dept} value={dept}>
+                {dept}
+              </option>
+            ))}
+          </select>
+        </div>
       </label>
       {errors?.dept && (
         <p>{errors?.dept?.message || "Department is required"}</p>
       )}
-      <input type="submit" value="Submit" disabled={!isValid} />
+
+      <div className={styles.submit}>
+        <input type="submit" value="Create" disabled={!isValid} />
+      </div>
     </form>
   )
 }
