e317059d2324d73393778e4ff5ee91b8f2d2de80
ZhannaZucher <jzucher@gmail.com>
2023-10-10 11:34:56 +0200

    formData type

---
+++ models/types.ts
@@ -0,0 +1,11 @@
+export type FormData = {
+  firstName: string
+  lastName: string
+  birthDate: Date
+  startDate: Date
+  street: string
+  city: string
+  state: string
+  zipCode: string
+  dept: string
+}
