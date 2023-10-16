8c1f5fda98607d7d17d5041019fc30999a04b990
ZhannaZucher <jzucher@gmail.com>
2023-10-16 16:42:29 +0200

    move modal handling into form.tsx

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useState } from "react"
 import { states } from "@/data/states"
 import { useForm, FormProvider } from "react-hook-form"
 import styles from "@/styles/Form.module.scss"
@@ -8,12 +9,11 @@ import { FormData } from "@/models/types"
 import FormDatePicker from "./FormDatePicker"
 import FormSelect from "./FormSelect"
 import { formatOptions } from "@/utils/formatData"
+import FormModal from "./FormModal"
 
-type FormProps = {
-  setActive: (value: boolean) => void
-}
+const Form = () => {
+  const [modalActive, setModalActive] = useState(false)
 
-const Form = ({ setActive }: FormProps) => {
   //object containing methods for registering components into React Hook Form
   const methods = useForm<FormData>({
     mode: "onBlur", //indicates when the error message appears
@@ -48,7 +48,7 @@ const Form = ({ setActive }: FormProps) => {
     }
     // alert(JSON.stringify(employee))
     console.log(employee)
-    setActive(true)
+    setModalActive(true)
     methods.reset()
   })
 
@@ -57,108 +57,113 @@ const Form = ({ setActive }: FormProps) => {
   const statesOptions = formatOptions(states)
 
   return (
-    <FormProvider {...methods}>
-      {/* pass all form methods into the context */}
-      <form onSubmit={onSubmit} className={styles.form}>
-        <div className={styles.wrapper}>
-          <div className={styles.flex}>
-            <FormInput
-              type="text"
-              label="First Name"
-              name="firstName"
-              errors={methods.formState.errors}
-              register={methods.register}
-              validationSchema={{
-                required: "This field is required",
-                pattern: {
-                  value: /[A-Za-z]{2}/,
-                  message: "Enter at least 2 caracters",
-                },
-              }}
-            />
-            <FormInput
-              type="text"
-              label="Last Name"
-              name="lastName"
-              errors={methods.formState.errors}
-              register={methods.register}
-              validationSchema={{
-                required: "This field is required",
-                pattern: {
-                  value: /[A-Za-z]{2}/,
-                  message: "Enter at least 2 caracters",
-                },
-              }}
-            />
-            <FormDatePicker
-              label="Date of Birth"
-              name="birthDate"
-              endWith={new Date()} //dates range ends with today's date
-            />
-            <FormDatePicker
-              label="Date of Start"
-              name="startDate"
-              startFrom={
-                birthDateInputValue ? methods.getValues("birthDate") : null
-              } //if the date of birth is already entered, dates to pick start from the date of birth OR null
+    <>
+      <FormProvider {...methods}>
+        {/* pass all form methods into the context */}
+        <form onSubmit={onSubmit} className={styles.form}>
+          <div className={styles.wrapper}>
+            <div className={styles.flex}>
+              <FormInput
+                type="text"
+                label="First Name"
+                name="firstName"
+                errors={methods.formState.errors}
+                register={methods.register}
+                validationSchema={{
+                  required: "This field is required",
+                  pattern: {
+                    value: /[A-Za-z]{2}/,
+                    message: "Enter at least 2 caracters",
+                  },
+                }}
+              />
+              <FormInput
+                type="text"
+                label="Last Name"
+                name="lastName"
+                errors={methods.formState.errors}
+                register={methods.register}
+                validationSchema={{
+                  required: "This field is required",
+                  pattern: {
+                    value: /[A-Za-z]{2}/,
+                    message: "Enter at least 2 caracters",
+                  },
+                }}
+              />
+              <FormDatePicker
+                label="Date of Birth"
+                name="birthDate"
+                endWith={new Date()} //dates range ends with today's date
+              />
+              <FormDatePicker
+                label="Date of Start"
+                name="startDate"
+                startFrom={
+                  birthDateInputValue ? methods.getValues("birthDate") : null
+                } //if the date of birth is already entered, dates to pick start from the date of birth OR null
+              />
+            </div>
+            <div className={styles.flex}>
+              <FormInput
+                type="text"
+                label="Street"
+                name="street"
+                errors={methods.formState.errors}
+                register={methods.register}
+                validationSchema={{
+                  required: "This field is required",
+                  pattern: {
+                    value: /[A-Za-z0-9]{5}/,
+                    message: "Enter at least 5 caracters",
+                  },
+                }}
+              />
+              <FormInput
+                type="text"
+                label="City"
+                name="city"
+                errors={methods.formState.errors}
+                register={methods.register}
+                validationSchema={{
+                  required: "This field is required",
+                  pattern: {
+                    value: /[A-Za-z]{2}/,
+                    message: "Enter at least 2 caracters",
+                  },
+                }}
+              />
+              <FormSelect label="State" name="state" options={statesOptions} />
+              <FormInput
+                type="number"
+                label="Zip Code"
+                name="zipCode"
+                errors={methods.formState.errors}
+                register={methods.register}
+                validationSchema={{
+                  required: "This field is required",
+                  pattern: {
+                    value: /[0-9]{5}/,
+                    message: "Enter 5 digits",
+                  },
+                }}
+              />
+            </div>
+          </div>
+          <FormSelect label="Department" name="dept" options={deptsOptions} />
+          <div className={styles.submit}>
+            <input
+              type="submit"
+              value="Create"
+              disabled={!methods.formState.isValid} //submit btn disabled until all form fields are valid
             />
           </div>
-          <div className={styles.flex}>
-            <FormInput
-              type="text"
-              label="Street"
-              name="street"
-              errors={methods.formState.errors}
-              register={methods.register}
-              validationSchema={{
-                required: "This field is required",
-                pattern: {
-                  value: /[A-Za-z0-9]{5}/,
-                  message: "Enter at least 5 caracters",
-                },
-              }}
-            />
-            <FormInput
-              type="text"
-              label="City"
-              name="city"
-              errors={methods.formState.errors}
-              register={methods.register}
-              validationSchema={{
-                required: "This field is required",
-                pattern: {
-                  value: /[A-Za-z]{2}/,
-                  message: "Enter at least 2 caracters",
-                },
-              }}
-            />
-            <FormSelect label="State" name="state" options={statesOptions} />
-            <FormInput
-              type="number"
-              label="Zip Code"
-              name="zipCode"
-              errors={methods.formState.errors}
-              register={methods.register}
-              validationSchema={{
-                required: "This field is required",
-                pattern: {
-                  value: /[0-9]{5}/,
-                  message: "Enter 5 digits",
-                },
-              }}
-            />
-          </div>
-        </div>
-        <FormSelect label="Department" name="dept" options={deptsOptions} />
-        <div className={styles.submit}>
-          <input
-            type="submit"
-            value="Create"
-            disabled={!methods.formState.isValid} //submit btn disabled until all form fields are valid
-          />
-        </div>
-      </form>
-    </FormProvider>
+        </form>
+      </FormProvider>
+      <FormModal active={modalActive} setActive={setModalActive}>
+        <p>Employee created !</p>
+      </FormModal>
+    </>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
-"use client"
-import { useState } from "react"
 import Form from "./components/Form"
-import FormModal from "./components/FormModal"
 
 export default function Home() {
-  const [modalActive, setModalActive] = useState(false)
   return (
     <main>
       <h2>Create Employee</h2>
-      <Form setActive={setModalActive} />
-      <FormModal active={modalActive} setActive={setModalActive}>
-        <p>Employee created !</p>
-      </FormModal>
+      <Form />
     </main>
   )
 }
