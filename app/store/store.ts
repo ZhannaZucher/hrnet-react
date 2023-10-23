36e5a28369fea23ff70a4dffe656c509b3623972
ZhannaZucher <jzucher@gmail.com>
2023-10-23 10:13:27 +0200

    add typed selector & dispatch

--- app/store/store.ts
+++ app/store/store.ts
@@ -2,6 +2,7 @@
 
 import { configureStore } from "@reduxjs/toolkit"
 import employeesReducer from "./employeesSlice"
+import { useSelector, TypedUseSelectorHook, useDispatch } from "react-redux"
 
 export const store = configureStore({
   reducer: {
@@ -9,5 +10,12 @@ export const store = configureStore({
   },
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+//typed useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+//typed useDispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+export const selectEmployees = (state: RootState) => state.employees.data
